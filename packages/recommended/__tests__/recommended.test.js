161ba61da1af427d8b11428edcf509e22f99ce6b
heiyehk <heiyehk@foxmail.com>
2023-08-29 13:10:46 +0800

    delete: recommended

--- packages/recommended/__tests__/recommended.test.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-const recommended = require('..');
-const assert = require('assert').strict;
-
-assert.strictEqual(recommended(), 'Hello from recommended');
-console.info('recommended tests passed');
